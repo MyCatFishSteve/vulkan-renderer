4dfbbbf2b6b9f1ddf17a2233b9e90fe4c65dcd14
Johannes <johannesschneider@outlook.com>
2021-01-19 00:53:33 +0100

    [collision] Added sorted collisions.

--- include/inexor/vulkan-renderer/world/collision.hpp
+++ include/inexor/vulkan-renderer/world/collision.hpp
@@ -18,11 +18,9 @@ private:
 
 public:
     CollisionData(std::shared_ptr<Cube> cube);
-    CollisionData(const CollisionData &) = delete;
-    CollisionData(CollisionData &&) = delete;
-
-    CollisionData &operator=(const CollisionData &) = delete;
-    CollisionData &operator=(CollisionData &&) = delete;
+    CollisionData();
+    void reset();
+    [[nodiscard]] float squared_distance(glm::vec3 position);
 };
 
 // TODO: Implement collision with multiple octree worlds.
@@ -30,6 +28,23 @@ class OctreeCollision {
 private:
     // TODO: Maybe change this to a const reference?
     const std::shared_ptr<world::Cube> m_cube;
+
+    /// Which cubes are hit?
+    /// This serves as a std::vector, but avoids memory allocation.
+    /// TODO: How many cubes can be hit as most? It is a number smaller than 8.
+    std::array<CollisionData, 8> m_hits;
+    std::uint32_t m_hits_found{0};
+
+    // This serves as a replacement for std::vector.clear();
+    void clear_hits();
+
+    /// @brief Adds an octree collision.
+    /// @param cube The cube.
+    void add_hit(CollisionData collision);
+
+    /// @brief Sort octree collision hits by distance from position's point.
+    /// @param position The origin point of the ray.
+    void sort_hits(glm::vec3 position);
 
     /// @brief ``True`` if the ray build from the two vectors collides with the cube's bounding sphere.
     /// @param position The start position vector of the ray.
@@ -49,16 +64,10 @@ public:
     /// @param world The octree world.
     OctreeCollision(std::shared_ptr<world::Cube> world);
 
-    OctreeCollision(const OctreeCollision &) = delete;
-    OctreeCollision(OctreeCollision &&) = delete;
-
-    OctreeCollision &operator=(const OctreeCollision &) = delete;
-    OctreeCollision &operator=(OctreeCollision &&) = delete;
-
     /// @brief Checks for a collision between a camera ray and octree geometry.
     /// @param position The camera's position.
     /// @param direction The camera's view direction.
     /// @return
-    std::optional<CollisionData> check_for_collision(glm::vec3 position, glm::vec3 direction) const;
+    std::optional<CollisionData> check_for_collision(glm::vec3 position, glm::vec3 direction);
 };
 } // namespace inexor::vulkan_renderer::world
--- src/vulkan-renderer/world/collision.cpp
+++ src/vulkan-renderer/world/collision.cpp
@@ -3,12 +3,25 @@
 #include <glm/gtx/intersect.hpp>
 #include <spdlog/spdlog.h>
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <unordered_map>
 
 namespace inexor::vulkan_renderer::world {
 
 CollisionData::CollisionData(std::shared_ptr<Cube> cube) : m_cube(cube) {}
+
+CollisionData::CollisionData() : m_cube(nullptr) {}
+
+void CollisionData::reset() {
+    m_cube = nullptr;
+}
+
+float CollisionData::squared_distance(glm::vec3 position) {
+    return std::pow(m_cube->center().x - position.x, 2) + std::pow(m_cube->center().y - position.y, 2) +
+           std::pow(m_cube->center().z - position.z, 2);
+}
 
 OctreeCollision::OctreeCollision(std::shared_ptr<Cube> cube) : m_cube(cube) {}
 
@@ -61,8 +74,31 @@ bool OctreeCollision::ray_box_collision(const std::array<glm::vec3, 2> box_bound
     return true;
 }
 
-std::optional<CollisionData> OctreeCollision::check_for_collision(const glm::vec3 position,
-                                                                  const glm::vec3 direction) const {
+void OctreeCollision::clear_hits() {
+    for (auto hit : m_hits) {
+        hit.reset();
+    }
+    m_hits_found = 0;
+}
+
+void OctreeCollision::add_hit(CollisionData collision) {
+    m_hits[m_hits_found] = collision;
+    m_hits_found++;
+}
+
+void OctreeCollision::sort_hits(const glm::vec3 position) {
+    // TODO: Refactor! This implementation is stupid. There must be a better way of doing this!
+    std::unordered_map<std::size_t, float> calculated_distances;
+    for (std::size_t i = 0; i < m_hits_found; i++) {
+        calculated_distances[i] = m_hits[i].squared_distance(position);
+        spdlog::trace("Collision {}, squared distance {}", i, calculated_distances[i]);
+    }
+
+    // TODO: Search for the lowest squared distance.
+    // TODO: Return sorted collisions.
+}
+
+std::optional<CollisionData> OctreeCollision::check_for_collision(const glm::vec3 position, const glm::vec3 direction) {
     /// If the cube is empty, a collision with a ray is not possible,
     /// and there are no sub cubes to check for collision either.
     if (m_cube->type() == Cube::Type::EMPTY) {
@@ -76,19 +112,26 @@ std::optional<CollisionData> OctreeCollision::check_for_collision(const glm::vec
         if (ray_box_collision(m_cube->bounding_box(), position, direction)) {
             if (m_cube->is_leaf()) {
                 /// This is a leaf node, a collision has been found.
-                /// TODO: Check which face is selected.
-                /// TODO: Check which edge is selected.
+                /// TODO: Check which face and edge is selected.
                 return std::make_optional<CollisionData>(m_cube);
             } else {
+                clear_hits();
                 /// Iterate through all sub cubes and check for collision.
                 const auto &subcubes = m_cube->childs();
                 for (std::int32_t i = 0; i < 8; i++) {
                     if (subcubes[i]->type() != Cube::Type::EMPTY) {
                         OctreeCollision subcollision(subcubes[i]);
-                        if (subcollision.check_for_collision(position, direction)) {
+                        const auto collision_result = subcollision.check_for_collision(position, direction);
+                        if (collision_result) {
                             spdlog::debug("Collided with subcube no {}.", i);
+                            add_hit(collision_result.value());
+                            // TODO: Check how many hits could be found at most (less than 8) and abort the loop.
                         }
                     }
+                }
+
+                if (m_hits_found > 1) {
+                    sort_hits(position);
                 }
             }
         }
